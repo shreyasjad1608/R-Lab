248a0ddfafa08680b92aaae699eb0c6c765553d5
Shreyas Jadhav <119096816+shreyasjad1608@users.noreply.github.com>
2024-01-28 22:51:04 +0530

    Update prog12.r

--- prog12.r
+++ prog12.r
@@ -1,32 +1,33 @@
 library(dplyr)
 library(ggplot2)
 
-df<-read.csv("C:/Users/dhruv/OneDrive/Desktop/Studies/SEM_5/R lab/data.csv")
-#task01
-str(df)
-summary(df)
+data<-read.csv("C:/Users/dhruv/OneDrive/Desktop/Studies/SEM_5/R lab/data.csv")
 
-#task02
-total_matches<-nrow(df)
-unique_matches<-length(unique(c(df$team1,df$team2,df$winner)))
+str(data)
+summary(data)
+
+total_matches<-nrow(data)
+unique_matches<-(unique(c(data$team1,data$team2,data$winner)))
 
 print(total_matches)
-print(unique_matches)
+print(length(unique_matches))
 
-#task03
-matches_won_per_team<-table(df$winner)
-avg_total_runs<-mean(as.numeric(df$win_by_runs,na.rm=TRUE))
-total_wickets<-mean(as.numeric(df$win_by_wickets,na.rm=TRUE))
-
+matches_won_per_team<-table(data$winner)
 print(matches_won_per_team)
-print(round(avg_total_runs,2))
-print(round(total_wickets,2))
 
-#task04
+avg_wickets<-round(mean(as.integer(data$win_by_wickets)),2)
+avg_runs<-round(mean(as.integer(data$win_by_runs)),2)
 
-most_freq_venue<-names(sort(table(df$venue),decreasing=TRUE)[1])
-print(most_freq_venue)
-matches_won_data <- as.data.frame(matches_won_per_team)
-matches_won_data$Team <- rownames(matches_won_data)
-barplot(matches_won_per_team, main = "Number of Matches Won by Each Team",
-        xlab = "Team", ylab = "Wins", col =rainbow(unique_matches))
+print(avg_wickets)
+print(avg_runs)
+
+venue<-names(sort(table(data$venue),decreasing = TRUE)[1])
+print(venue)
+
+matches_won<-as.data.frame(matches_won_per_team)
+print(matches_won)
+matches_won$data<-rownames(matches_won)
+print(matches_won)
+
+bar<-ggplot(matches_won,aes(x=unique_matches,y=Freq,fill=unique_matches))+geom_bar(stat = "identity")+labs(title="bar plot")
+print(bar)
